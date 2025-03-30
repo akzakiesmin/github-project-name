e35fd5f8225fda99eacbbc7a9691771eaf447421
akzakiesmin <akzakiesmin@gmail.com>
2025-03-30 17:46:56 +0000

    Add 19.php

---
+++ src/19.php
@@ -0,0 +1,19 @@
+<?php
+// Example PHP code to demonstrate basic functionalities like accessing variables and loops
+
+// Accessing variable
+$variable = "Hello, World!";
+echo $variable;
+
+// Looping through arrays
+$array = [1, 2, 3, 4, 5];
+foreach ($array as $value) {
+    echo $value . " ";
+}
+
+// Example of a simple if-else statement
+if (true) {
+    echo "This is true";
+} else {
+    echo "This is false";
+}
